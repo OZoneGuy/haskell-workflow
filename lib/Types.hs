02929e53fc48bd6a4a1c07433bc5c62cd83ff73b
OZoneGuy <oalkersh@protonmail.com>
2022-02-19 22:50:00 -0500

    feat - Added input to jobs and improved Type
    Jobs may now accept a list of inputs
    Type gets the same treatment as resources

--- lib/Types.hs
+++ lib/Types.hs
@@ -30,7 +30,9 @@ data Job = Job
     -- | The jobs this jobs depends on
     jobDependencies :: [Job],
     -- | The resources required by this job
-    resDep :: [Resource]
+    resDep :: [Resource],
+    -- | The expected input for the job
+    input :: [Type]
   }
 
 defaultJob :: Job
@@ -40,10 +42,15 @@ defaultJob =
       language = "bash",
       body = "echo \"Default job output...\"",
       jobDependencies = [],
-      resDep = []
+      resDep = [],
+      input = []
     }
 
-data Execution = Parallel [Job] | Sequence [Job]
+data Execution = Parallel {jobs :: [Job]} | Sequence {jobs :: [Job]}
+
+class TypeClass a where
+
+data Type = forall a. TypeClass a => Type a
 
 data NewType = NewType
   { name :: String,
